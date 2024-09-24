70414dbc7405b9ec77c005a6592854b7b2d4408a
lanskaianadezhda <nadin-91@bk.ru>
2024-09-24 15:17:01 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -33,21 +33,60 @@ public class Main {
         // task 4
         System.out.println("Зачача 4");
 
-        int age1 = 23;
-        if (age1 >= 2 && age1 <= 6) {
-            System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить в детский сад");
-            if (age1 >= 7 && age1 <= 17) {
-                System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить в школу");
-                if (age1 >= 18 && age1 <= 24) {
-                    System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить в университет");
-                    if (age1 > 24) {
-                        System.out.println("Если возраст человека равен " + age1 + " , то ему нужно ходить  на работу");
-                    }
+        int years = 23;
+        if (years >= 2 && years <= 6) {
+            System.out.println("Если возраст человека равен " + years + " , то ему нужно ходить в детский сад");
+        } else if (years >= 7 && years <= 17) {
+            System.out.println("Если возраст человека равен " + years + " , то ему нужно ходить в школу");
+        } else if (years >= 18 && years <= 24) {
+            System.out.println("Если возраст человека равен " + years + " , то ему нужно ходить в университет");
+        } else if (years > 24) {
+            System.out.println("Если возраст человека равен " + years + " , то ему нужно ходить  на работу");
+        }
 
+        //task 5
+        System.out.println("Задача 5");
 
-                    //task 5
-                    System.out.println("Задача 5");
-                }
+        age = 8;
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему нельзя кататься на аттракционе");
+        }
+        if (age >= 5 && age < 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе  в сопровождении взрослого");
+        }
+        if (age >= 14) {
+            System.out.println("Если возраст ребенка равен " + age + ", то ему можно кататься на аттракционе  без сопр0овождения взрослого");
+        }
+
+        // task 6
+        System.out.println("Задача 6");
+
+        int places = 102;
+        int sitPlaces = 60;
+        int people = 48;
+        if (people < sitPlaces) {
+            System.out.println("В вагоне есть сидячие места");
+        }
+        if (people >= sitPlaces && people < places) {
+            System.out.println("В вагоне есть стоячие места");
+        }
+        if (people >= places) {
+            System.out.println("В вагоне нет мест");
+        }
+
+        // task 7
+        System.out.println("Зачача 7");
+
+        int one = 11;
+        int two = 7;
+        int three = 52;
+        if (one >= two && one >= three) {
+            System.out.println("Максимальное число в переменной one равно " + one);
+        } else {
+            if (two >= three) {
+                System.out.println("Максимальное число в переменной two равно " + two);
+            } else {
+                System.out.println("Максимальное число в переменной three равно " + three);
             }
         }
     }
